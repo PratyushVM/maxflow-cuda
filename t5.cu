6c8f4f7a7a1b5d1b68268f27cbf28058f06223a5
PratyushMoorthy <pratyush.v.m@gmail.com>
2020-07-21 19:49:43 +0530

    add pushrelabel kernel 2 in t5

--- t5.cu
+++ t5.cu
@@ -97,83 +97,85 @@ __global__ void push_relabel_kernel(int V, int *gpu_height, int *gpu_excess_flow
     // u'th node is operated on by the u'th thread
     unsigned int u = (blockIdx.x*blockDim.x) + threadIdx.x;
 
-    // cycle value is set to KERNEL_CYCLES as required 
-    int cycle = KERNEL_CYCLES;
-
-    /* Variables declared to be used inside the kernel :
-     * e_dash - initial excess flow of node u
-     * h_dash - height of lowest neighbor of node u
-     * h_double_dash - used to iterate among height values to find h_dash
-     * v - used to iterate among nodes to find v_dash
-     * v_dash - lowest neighbor of node u 
-     * d - flow to be pushed from node u
-     */
-
-    int e_dash,h_dash,h_double_dash,v,v_dash,d;
-
-    while(cycle > 0)
+    if(u < V)
     {
-        if( (gpu_excess_flow[u] > 0) && (gpu_height[u] < V) )
-        {
-            e_dash = gpu_excess_flow[u];
-            h_dash = INF;
+        // cycle value is set to KERNEL_CYCLES as required 
+        int cycle = KERNEL_CYCLES;
 
-            for(v = 0; v < V; v++)
+        /* Variables declared to be used inside the kernel :
+        * e_dash - initial excess flow of node u
+        * h_dash - height of lowest neighbor of node u
+        * h_double_dash - used to iterate among height values to find h_dash
+        * v - used to iterate among nodes to find v_dash
+        * v_dash - lowest neighbor of node u 
+        * d - flow to be pushed from node u
+        */
+
+        int e_dash,h_dash,h_double_dash,v,v_dash,d;
+
+        while(cycle > 0)
+        {
+            if( (gpu_excess_flow[u] > 0) && (gpu_height[u] < V) )
             {
-                // for all (u,v) belonging to E_f (residual graph edgelist)
-                if(cpu_rflowmtx[IDX(u,v)] > 0)
+                e_dash = gpu_excess_flow[u];
+                h_dash = INF;
+
+                for(v = 0; v < V; v++)
                 {
-                    h_double_dash = gpu_height[v];
-                    // finding lowest neighbor of node u
-                    if(h_double_dash < h_dash)
+                    // for all (u,v) belonging to E_f (residual graph edgelist)
+                    if(cpu_rflowmtx[IDX(u,v)] > 0)
                     {
-                        v_dash = v;
-                        h_dash = h_double_dash;
+                        h_double_dash = gpu_height[v];
+                        // finding lowest neighbor of node u
+                        if(h_double_dash < h_dash)
+                        {
+                            v_dash = v;
+                            h_dash = h_double_dash;
+                        }
                     }
                 }
+
+                if(gpu_height[u] > h_dash)
+                {
+                    /* height of u > height of lowest neighbor
+                    * Push operation can be performed from node u to lowest neighbor
+                    * All addition, subtraction and minimum operations are done using Atomics
+                    * This is to avoid anomalies in conflicts between multiple threads
+                    */
+
+                    // d captures flow to be pushed 
+                    d = e_dash;
+                    atomicMin(&d,gpu_rflowmtx[IDX(u,v_dash)]);
+
+                    // Residual flow towards lowest neighbor from node u is increased
+                    atomicAdd(&cpu_rflowmtx[IDX(v_dash,u)],d);
+
+                    // Residual flow towards node u from lowest neighbor is decreased
+                    atomicSub(&cpu_rflowmtx[IDX(u,v_dash)],d);
+
+                    // Excess flow of lowest neighbor and node u are updated
+                    atomicAdd(&gpu_excess_flow[v_dash],d);
+                    atomicSub(&gpu_excess_flow[u],d);
+                }
+
+                else
+                {
+                    /* height of u <= height of lowest neighbor,
+                    * No neighbor with lesser height exists
+                    * Push cannot be performed to any neighbor
+                    * Hence, relabel operation is performed
+                    */
+
+                    gpu_height[u] = h_dash + 1;
+                }
+
             }
 
-            if(gpu_height[u] > h_dash)
-            {
-                /* height of u > height of lowest neighbor
-                 * Push operation can be performed from node u to lowest neighbor
-                 * All addition, subtraction and minimum operations are done using Atomics
-                 * This is to avoid anomalies in conflicts between multiple threads
-                 */
-
-                // d captures flow to be pushed 
-                d = e_dash;
-                atomicMin(&d,gpu_rflowmtx[IDX(u,v_dash)]);
-
-                // Residual flow towards lowest neighbor from node u is increased
-                atomicAdd(&cpu_rflowmtx[IDX(v_dash,u)],d);
-
-                // Residual flow towards node u from lowest neighbor is decreased
-                atomicSub(&cpu_rflowmtx[IDX(u,v_dash)],d);
-
-                // Excess flow of lowest neighbor and node u are updated
-                atomicAdd(&gpu_excess_flow[v_dash],d);
-                atomicSub(&gpu_excess_flow[u],d);
-            }
-
-            else
-            {
-                /* height of u <= height of lowest neighbor,
-                 * No neighbor with lesser height exists
-                 * Push cannot be performed to any neighbor
-                 * Hence, relabel operation is performed
-                 */
-
-                gpu_height[u] = h_dash + 1;
-            }
+            // cycle value is decreased
+            cycle = cycle - 1;
 
         }
-
-        // cycle value is decreased
-        cycle = cycle - 1;
-
     }
-
 }
 
 void push_relabel(int V, int source, int sink, int *cpu_height, int *cpu_excess_flow, int *cpu_adjmtx, int *cpu_rflowmtx, int *Excess_total, int *gpu_height, int *gpu_excess_flow, int *gpu_adjmtx, int *gpu_rflowmtx)
