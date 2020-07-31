fa2a81f07a85b7c07c84ceefecbac77cc63f8dca
PratyushMoorthy <pratyush.v.m@gmail.com>
2020-07-31 10:33:05 +0530

    Add Working version of maxflow.cu

--- maxflow.cu
+++ maxflow.cu
@@ -10,6 +10,262 @@
 #define IDX(x,y) ( ( (x)*(number_of_nodes) ) + (y) )
 #define KERNEL_CYCLES V
 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int,int>
+#define mp make_pair
+#define ff first
+#define ss second
+
+// Structure representing a Vertex
+struct Vertex
+{
+    int height; // height of node
+    int ex_flow; // excess flow 
+
+    // constructor function
+    Vertex(int h, int e)
+    {
+        this->height = h;
+        this->ex_flow = e;
+    }
+};
+
+// Structure representing an Edge
+struct Edge_s
+{
+    int u,v; // edge from node u to node v
+    int flow; // current flow
+    int capacity; // capacity of edge
+
+    // constructor function 
+    Edge_s(int f, int c, int a, int b)
+    {
+        this->u = a;
+        this->v = b;
+        this->capacity = c;
+        this->flow = f;
+    }
+};
+
+class Graph_s
+{
+    int V; // number of vertices
+    vector<Vertex> vertex; // vector of vertices
+    vector<Edge_s> edge; // vector of edges
+
+    // function to push excess flow from u
+    bool push(int u);
+
+    // function to relabel a vertex u
+    void relabel(int u);
+
+    // function to initialize preflow
+    void preflow(int s);
+
+    // function to reverse edge
+    void updatereverseflow(int i, int flow);
+
+public:
+    Graph_s(int v); // constructor to create graph with v vertices
+
+    void addedge(int u, int v, int w); // function to add an edge
+
+    int maxflow(int s, int t); // function that returns maximum flow from source s to sink t
+
+};
+
+Graph_s::Graph_s(int v)
+{
+    this->V = v;
+
+    // all vertices are initialized with zero height and excess flow
+    for(int i = 0; i < V; i++)
+    {
+        vertex.pb(Vertex(0,0));
+    }
+}
+
+void Graph_s::addedge(int u, int v, int capacity)
+{
+    // flow is initially 0 for all edges
+    edge.pb(Edge_s(0,capacity,u,v));
+}
+
+void Graph_s::preflow(int s)
+{
+    // making height of source vertex equal to number of vertices
+    // height of other vertices are 0 by default
+    vertex[s].height = vertex.size();
+
+    for(int i = 0; i < edge.size(); i++)
+    {
+        // if current edge goes from source
+        if(edge[i].u == s)
+        {
+            // flow is equal to capacity
+            edge[i].flow = edge[i].capacity;
+
+            // initialize excess flow for adjacent vertices
+            vertex[edge[i].v].ex_flow += edge[i].flow;
+
+            // add reverse edge in residual graph with capacity equal to 0
+            edge.pb(Edge_s(-edge[i].flow,0,edge[i].v,s));
+        }
+    }
+}
+
+// function that returns index of overflowing Vertex
+int overflowvertex(vector<Vertex>& ver, int s, int t)
+{
+    for(int i = 0; i < ver.size(); i++)
+    {
+        if( i != s && i != t && ver[i].ex_flow > 0 )
+        return i;
+    }
+
+    // return -1 if no overflowing vertex exists
+    return -1;
+}
+
+// Update reverse flow for flow added on i-th edge
+void Graph_s::updatereverseflow(int i, int flow)
+{
+    int u = edge[i].v , v = edge[i].u;
+
+    for(int j = 0; j < edge.size(); j++)
+    {
+        if(edge[j].v == v && edge[j].u == u)
+        {
+            edge[j].flow -= flow;
+            return;
+        }
+    }
+
+    // if reverse edge not present in residual graph
+    edge.pb(Edge_s(0,flow,u,v));
+}
+
+// To push flow from overflowing vertex u
+bool Graph_s::push(int u)
+{
+    // Traverse through all edges to find an adjacent vertex of u, to which flow can be pushed
+    for(int i = 0; i < edge.size(); i++)
+    {
+        if(edge[i].u == u)
+        {
+            // if flow is equal to capacity then no push is possible
+            if(edge[i].flow == edge[i].capacity)
+            continue;
+
+            // checking if height of adjacent vertex is smaller than height of overflowing vertex
+            if(vertex[u].height > vertex[edge[i].v].height)
+            {
+                // flow to be pushed is equal to minimum of remaining flow on edge and excess flow
+                int flow = min(edge[i].capacity - edge[i].flow, vertex[u].ex_flow);
+
+                // reduce excess flow for overflowing vertex
+                vertex[u].ex_flow -= flow;
+
+                // increase excess flow for adjacent vertex
+                vertex[edge[i].v].ex_flow += flow;
+
+                // add residual flow 
+                edge[i].flow += flow;
+
+                updatereverseflow(i,flow);
+
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+// function to relabel vertex u
+void Graph_s::relabel(int u)
+{
+    // initialize mimimum height of an adjacent
+    int mh = INT_MAX;
+
+    // find adjacent with lowest height
+    for(int i = 0; i < edge.size(); i++)
+    {
+        if(edge[i].u == u)
+        {
+            // if flow is equal to capacity then no relabeling
+            if(edge[i].flow == edge[i].capacity)
+            continue;
+
+            // update minimum height
+            if(vertex[edge[i].v].height < mh)
+            {
+                mh = vertex[edge[i].v].height;
+
+                // updating height of u
+                vertex[u].height = mh + 1; 
+            }
+        }
+    }
+}
+
+// function to print the maximum flow from source s to sink t
+int Graph_s::maxflow(int s, int t)
+{
+    preflow(s);
+
+    while(overflowvertex(vertex,s,t) != -1)
+    {
+        int u = overflowvertex(vertex,s,t);
+        if(!push(u))
+        {
+            relabel(u);
+        }
+    }
+
+    // ex_flow of the sink will be final maximum flow
+    return vertex[t].ex_flow;
+}
+
+// Driver program to test above functions 
+int check(int V, int E, int source, int sink) 
+{ 
+    
+    Graph_s g(V);   
+
+    FILE *fp = fopen("edgelist.txt","r");
+
+    char buf1[10],buf2[10],buf3[10];
+    int e1,e2,cp;
+
+    for(int i = 0; i < E; i++)
+    {
+        fscanf(fp,"%s",buf1);
+        fscanf(fp,"%s",buf2);
+        fscanf(fp,"%s",buf3);
+
+        e1 = atoi(buf1);
+        e2 = atoi(buf2);
+        cp = atoi(buf3);
+
+        g.addedge(e1,e2,cp);
+    }
+  
+    // Initialize source and sink 
+    int s = source, t = sink; 
+  
+    //cout << "Maximum flow is " << g.maxflow(s, t); 
+    return g.maxflow(s,t) ; 
+} 
+
+
+
+
+
 void print(int V,int *cpu_height, int *cpu_excess_flow, int *cpu_rflowmtx, int *cpu_adjmtx)
 {
     printf("\nHeight :");
@@ -442,7 +698,19 @@ int main(int argc, char **argv)
     push_relabel(V,source,sink,cpu_height,cpu_excess_flow,cpu_adjmtx,cpu_rflowmtx,Excess_total,gpu_height,gpu_excess_flow,gpu_adjmtx,gpu_rflowmtx);
     
     // print value
-    printf("The maximum flow value of this flow network is %d\n",cpu_excess_flow[sink]);
+    int serial_check = check(V,E,source,sink);
+
+    printf("The maximum flow value of this flow network as calculated by the parallel implementation is %d\n",cpu_excess_flow[sink]);
+    printf("The maximum flow of this flow network as calculated by the serial implementation is %d\n",serial_check);
+    
+    if(cpu_excess_flow[sink] == serial_check)
+    {
+        printf("Passed correctness check\n");
+    }
+    else
+    {
+        printf("Failed correctness check\n");
+    }
 
     // free device memory
     cudaFree(gpu_height);
